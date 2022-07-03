092e694dc407e2c70d2597ea5970bd330742b1d7
shivansh977 <83871363+shivansh977@users.noreply.github.com>
2022-07-03 10:28:14 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ export const GOOGLE_CLIENT_ID= process.env.GOOGLE_CLIENT_ID || '960407959948-ssp
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || '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'
 export const SERVER_ROOT_URI = 'http://localhost:4000'
 export const UI_ROOT_URI = 'http://localhost:3000'
-export const JWT_SECRET = 'sssh'
+export const JWT_SECRET = 'bnjabhai'
 export const COOKIE_NAME = 'auth_token'
